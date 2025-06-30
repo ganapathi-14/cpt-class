65c1e07b949158d41726764ac61015650c1853aa
ganapathi-14 <asgsrinu2@gmail.com>
2025-06-30 12:24:14 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,6 @@
+import math
+num=int(input("Enter te number : "))
+print("Squre root " , math.sqrt(num))
+print("Factorial " , math.factorial(num))
+print("Pi value",math.pi)
+print("Pi operation",math.pi*2)
